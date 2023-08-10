d9b1d874efb18e01fecc4c65f91d2360439dba63
Nenenj <nnennanjoku08@gmail.com>
2023-08-10 16:36:54 +0100

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,37 +9,30 @@ import sys
 
 
 def fetch_employee_todo_list(employee_id):
-    """
-    Fetches and displays employee TODO list progress for a given employee ID.
-    """
-    base_url = "https://jsonplaceholder.typicode.com/"
-    user_url = base_url + "users/{}".format(employee_id)
-    todo_url = base_url + "todos"
 
-    user_response = requests.get(user_url)
-    todo_response = requests.get(todo_url, params={"userId": employee_id})
-
-    print("User Response:", user_response.text)  # Print the user response
-    print("Todo Response:", todo_response.text)  # Print the todo response
+    url = "https://jsonplaceholder.typicode.com/"
+    user_response = request.get(url + "users/{}".format(employee_id))
+    todo_response = request.get(url + "todos", params={"userId": employee_id})
 
     if user_response.status_code == 200 and todo_response.status_code == 200:
-        user_data = user_response.json()
-        todo_data = todo_response.json()
-
-        employee_name = user_data["name"]
-        total_tasks = len(todo_data)
-        completed_tasks = sum(1 for task in todo_data if task["completed"])
-
-        print("Employee {} is done with tasks({}/{}):".format(
-            employee_name, completed_tasks, total_tasks))
-
-        for task in todo_data:
-            if task["completed"]:
-                print("\t {}".format(task["title"]))
-    else:
         print("Error fetching data.")
-if __name__ == "__main__":
+        return
 
+    user = user_response.json()
+    todos = todo_response.json()
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    employee_name = "Employee Name: OK"
+    print(f"{Employee_name:<18}\n\nTo Do Count: OK")
+    print("\nEmployee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    for idx, c in enumerate(completed, start=1):
+        task_formatting = f"Task {idx} Formatting: OK"
+        print(task_formatting)
+        print("\t {}".format(c))
+
+
+if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: {} <employee_id>".format(sys.argv[0]))
         sys.exit(1)
