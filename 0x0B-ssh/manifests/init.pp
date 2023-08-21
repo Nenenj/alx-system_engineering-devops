91f034c4d00aadeb452c4684b59d05a4d545ef4b
Nenenj <nnennanjoku08@gmail.com>
2023-08-21 17:10:06 +0100

    update

---
+++ 0x0B-ssh/manifests/init.pp
@@ -0,0 +1,11 @@
+my_module/
+├── manifests/
+│   └── init.pp          # Main manifest file
+├── files/
+├── templates/
+├── lib/
+├── facts.d/
+├── tasks/
+├── plans/
+├── metadata.json       # Module metadata
+└── README.md           # Documentation
