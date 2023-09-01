fd8e53e3e11c2ba86367b69a26e08a24d0dc6595
Nenenj <nnennanjoku08@gmail.com>
2023-09-01 16:56:46 +0100

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,13 +1,6 @@
 # Puppet manifest to fix a bug in wp-setings.php
 
-file { '/var/www/html/wp-settings.php':
-  ensure => 'file',
-  content => 'Your file content goes here', 
-  mode => '0644',
-}
-
-exec { 'fix wordpress':
+exec { 'fix the php extension issue':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/',
-  require => File['/var/www/html/wp-settings.php'],
+  path    => '/usr/local/bin/:/bin/'
 }
