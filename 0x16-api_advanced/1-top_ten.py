32dd937b5e1c4bb91e19eb6354a5a010c144e80b
Nenenj <nnennanjoku08@gmail.com>
2023-09-25 08:58:01 +0100

    alx-system_engineering-devps

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,48 +1,21 @@
 #!/usr/bin/python3
-"""
-A function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit.
-"""
-
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts listed for a subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        None
-    """
-    base_url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     params = {
         "limit": 10
     }
-    response = requests.get(base_url, headers=headers, allow_redirects=False)
-
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
     if response.status_code == 404:
         print("None")
         return
-
-    data = response.json()
-    posts = data.get("data", {}).get("children", [])
-    for post in posts:
-        title = post.get("data", {}).get("title")
-        if title:
-            print(title)
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <subreddit>".format(sys.argv[0]))
-        sys.exit(1)
-
-    subreddit_name = sys.argv[1]
-    top_ten(subreddit_name)
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
