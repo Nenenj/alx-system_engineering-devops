3b0784d63f156c1b162c0299de1a521fef315ba9
Nenenj <nnennanjoku08@gmail.com>
2024-03-22 11:00:42 +0100

    delete

--- 0x0A-configuration_management/kill_process.pp
+++
@@ -1,6 +0,0 @@
-exec { 'killmenow_process':
-  command     => 'pkill -f killmenow',
-  path        => '/usr/bin:/bin', # Add additional paths as needed
-  onlyif      => 'pgrep -f killmenow',
-  refreshonly => true,
-}
