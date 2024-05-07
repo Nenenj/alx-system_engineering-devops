7faceb10982a8a25a1b42b727a25ce751908c4ac
Nenenj <nnennanjoku08@gmail.com>
2024-05-07 18:05:21 +0100

    updated api_advanced

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""A recursive function that queries the Reddit
-API and returns a list containing the titles of
-all hot articles for a given subreddit."""
+"""Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    """returns a list containing the titles
-    of all hot articles for a given subreddit"""
+    """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
@@ -30,4 +27,4 @@ def recurse(subreddit, hot_list=[], after="", count=0):
 
     if after is not None:
         return recurse(subreddit, hot_list, after, count)
-    return hot_list
+    return hot_list 
