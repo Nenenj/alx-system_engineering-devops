c164a44e1386f6f3b1aaebe326992811bf9ef627
Nenenj <nnennanjoku08@gmail.com>
2024-05-09 17:56:53 +0100

    webstack_monitoring

--- 0x14-mysql/setup.sql
+++ 0x14-mysql/setup.sql
@@ -8,5 +8,5 @@ GRANT SELECT ON tyrell_corp.nexus6 TO holberton_user@localhost;
 CREATE USER replica_user@'%' IDENTIFIED BY "omega1986";
 GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
 GRANT SELECT ON mysql.user TO holberton_user@localhost;
-CREATE USER web02@54.165.223.126 IDENTIFIED BY "web02";
-GRANT REPLICATION SLAVE ON *.* TO web02@54.165.223.126;
+CREATE USER web02@3.80.18.10 IDENTIFIED BY "web02";
+GRANT REPLICATION SLAVE ON *.* TO web02@3.80.18.10;
